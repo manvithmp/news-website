586ad07dc0f4a755825d231e2460d92d92a428cb
manvithmp <manvithmp13@gmail.com>
2025-02-13 19:43:57 +0530

    new commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+    reactStrictMode: true,
+    env: {
+      NEXT_PUBLIC_NEWS_API_KEY: process.env.NEXT_PUBLIC_NEWS_API_KEY,
+    },
+    trailingSlash: true,
+  };
